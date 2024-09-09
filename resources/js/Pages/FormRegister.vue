d603be6a2380cf0bf2810fbbf92747fad112c23d
jstalinko <alinkodotid@gmail.com>
2024-09-09 22:18:07 +0700

    add event

--- resources/js/Pages/FormRegister.vue
+++ resources/js/Pages/FormRegister.vue
@@ -88,7 +88,7 @@
                         ></textarea>
                     </div>
 
-                    <div class="text-center">
+                    <div class="text-center mb-10">
                         <button
                             type="submit"
                             class="bg-red-500 text-white px-6 py-2 rounded-lg hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-red-500"
@@ -96,6 +96,7 @@
                             Daftar
                         </button>
                     </div>
+                    <br><br><br>
                 </form>
             </div>
         </div>
